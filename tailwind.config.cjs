9199828371351364f8234d3a62bf732061be2ddb
aboseman1 <37965157+aboseman1@users.noreply.github.com>
2023-05-31 22:24:48 -0400

    Update tailwind.config.cjs
    Add Search UI React Tailwinds Config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
+const { ComponentsContentPath } = require("@yext/search-ui-react");
+
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    // this will apply the Tailwind styles to the components
+    ComponentsContentPath,
+  ],
   theme: {
     extend: {
       colors: {
         orange: "#ff9500",
         "dark-orange": "#db8000",
-      },
-      scale: {
-        1.02: "1.02",
       },
     },
   },
